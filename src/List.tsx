e32d8ad9c9f94f2f2485913a9df3333d90afb3f2
Justin <kidjustinchoi@gmail.com>
2022-10-05 19:40:08 +0900

    push data into db

--- src/List.tsx
+++ src/List.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IToDo, ToDos } from "./api";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
 interface IProps {
     todos: ToDos[];
@@ -9,17 +11,28 @@ interface IProps {
     query: string;
 };
 
+const Input = styled.input`
+
+`;
+
 function List({ todos, part, id, title }: IProps) {
     return (
-        <div>
-            <h1>{title}</h1>
-            {todos.map((todo: ToDos) => (
-                <>
-                    <h2>{todo.제목}</h2>
-                    {/* <h2>{todo.날짜}</h2> */}
-                </>
-            ))}
-        </div>
+        <>
+            <div>
+                <h1>{title}</h1>
+                {todos.map((todo: ToDos) => (
+                    <>
+                        <h2>{todo.제목}</h2>
+                        {/* <h2>{todo.날짜}</h2> */}
+                    </>
+                ))}
+            </div>
+            <form action="/add" method="POST">
+                <Input type="text" name="title" />
+                <Input type="text" name="date" />
+                <button>추가</button>
+            </form>
+        </>
     )
 }
 
