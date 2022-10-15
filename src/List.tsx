66d2e01a1ad44f2fca83d12c6ba538136ab2524a
Justin <kidjustinchoi@gmail.com>
2022-10-15 14:32:38 +0900

    fix the err

--- src/List.tsx
+++ src/List.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { IToDo, ToDos } from "./api";
 import { Link } from 'react-router-dom';
 import styled from "styled-components";
+import axios from "axios";
 
 interface IProps {
     todos: ToDos[];
@@ -17,30 +18,33 @@ const Input = styled.input`
 
 function List({ todos, part, id, title }: IProps) {
     const onClick = (e: any) => {
-        let writeCount = e.target.dataset.id; // 클릭한 요소의 data-id값을 가져옴
-        let nowClick = e.target; // 클릭한 요소를 가져옴
-        $.ajax({
-            method: "DELETE",
-            url: "delete",
-            data: { _id: writeCount },
-        }).done(() => {
-            nowClick.parent('li').fadeOut(); // 클릭한 요소의 부모요소를 삭제
-            console.log("삭제완료");
-        }).fail((xhr, textStatus, errorThrown) => {
-            console.log(xhr, textStatus, errorThrown);
-        })
-    };
+        let writeCount = e.target.dataset.id; //
+        let nowClick = e.target;
+        axios.delete('/delete', {
+            data: { _id: writeCount }
+        }).then(res => {
+            nowClick.parentElement.style.display = 'none'; // 
+            console.log('성공했지롱');
+        }).catch(err => {
+            console.log('헉 뭔가 잘못됐지롱')
+            console.log(err);
+        });
+    }
+
+
     return (
         <>
             <div>
                 <h1>{title}</h1>
-                {todos.map((todo: ToDos) => (
-                    <>
-                        <h2>{todo._id}: {todo.제목}</h2>
-                        <p>{todo.날짜}</p>
-                        <button data-id={todo._id} onClick={onClick}>삭제</button>
-                    </>
-                ))}
+                <ul>
+                    {todos.map((todo: ToDos) => (
+                        <li>
+                            <h2>{todo._id}: {todo.제목}</h2>
+                            <p>{todo.날짜}</p>
+                            <button data-id={todo._id} onClick={onClick}>삭제</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
             <form action="/add" method="POST">
                 <Input type="text" name="title" />
